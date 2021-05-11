cd0cdfe42f89fd08b070f0b6b6e4fd647d44633b
Nina19980108 <ninagirl8787@gmail.com>
2021-05-11 11:36:51 +0800

    feat: modify home route

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
+const record = require('./models/record')
 const Record = require('./models/record')
 
 const port = 3000
@@ -19,7 +20,20 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
-  res.render('index')
+  let totalAmount = 0
+  Record.find()
+    .lean()
+    .then(record => {
+      record.forEach(record => {
+        totalAmount += record.amount
+      })
+    })
+    .catch(error => console.error(error))
+
+  Record.find()
+    .lean()
+    .then(record => res.render('index', { record, totalAmount }))
+    .catch(error => console.error(error))
 })
 
 app.listen(port, () => {
