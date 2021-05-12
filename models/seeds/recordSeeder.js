21ce9c13b652d871c477f5222aa48ef2bde8b7f1
Nina19980108 <ninagirl8787@gmail.com>
2021-05-12 13:29:16 +0800

    feat: modify recordSeeder.js

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -8,11 +8,11 @@ db.on('error', () => {
 })
 db.once('open', () => {
   console.log('mongodb connected!')
-  for (let i = 0; i < 10; i++) {
+  for (let i = 1; i <= 10; i++) {
     Record.create({
       name: 'name' + i,
-      category: 'category' + i,
-      date: '2020-05-' + i,
+      category: 'fa-home',
+      date: '2020-05-10',
       amount: 30
     })
   }
