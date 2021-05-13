1ba579a763640bf03fe27a7852c9c9ebcdcf737e
Nina19980108 <ninagirl8787@gmail.com>
2021-05-13 14:38:14 +0800

    feat: add category display function

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
-const record = require('./models/record')
 const Record = require('./models/record')
 const bodyParser = require('body-parser')
 
@@ -97,6 +96,44 @@ app.post('/delete/:id', (req, res) => {
   return Record.findById(id)
     .then(record => record.remove())
     .then(() => res.redirect('/'))
+    .catch(error => console.error(error))
+})
+
+app.get('/category', (req, res) => {
+  const category = req.query.category
+
+  let fa_home = false
+  let fa_shuttle_van = false
+  let fa_grin_beam = false
+  let fa_utensils = false
+  let fa_pen = false
+
+  if (category === 'fa-home') {
+    fa_home = true
+  } else if (category === 'fa-shuttle-van') {
+    fa_shuttle_van = true
+  } else if (category === 'fa-grin-beam') {
+    fa_grin_beam = true
+  } else if (category === 'fa-utensils') {
+    fa_utensils = true
+  } else if (category === 'fa-pen') {
+    fa_pen = true
+  }
+
+  const query = new RegExp(category)
+  let totalAmount = 0
+  Record.find({ $or: [{ category: query }] })
+    .lean()
+    .then(record => {
+      record.forEach(record => {
+        totalAmount += record.amount
+      })
+    })
+    .catch(error => console.error(error))
+
+  return Record.find({ $or: [{ category: query }] })
+    .lean()
+    .then(record => res.render('index', { record, totalAmount, fa_home, fa_shuttle_van, fa_grin_beam, fa_utensils, fa_pen }))
     .catch(error => console.error(error))
 })
 
