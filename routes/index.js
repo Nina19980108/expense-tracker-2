7d29e086f8c6fc5bf30570fda36bc9a03c409443
Nina19980108 <ninagirl8787@gmail.com>
2021-05-14 09:11:08 +0800

    feat: modify category page

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,9 @@ const router = express.Router()
 const home = require('./modules/home')
 const New = require('./modules/new')
 const edit = require('./modules/edit')
-const category = require('./modules/category')
 
 router.use('/', home)
 router.use('/new', New)
 router.use('/edit', edit)
-router.use('/category', category)
 
 module.exports = router
