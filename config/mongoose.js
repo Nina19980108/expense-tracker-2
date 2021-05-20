55f0261f68ee6d86e9210212ed29a6ff968d1fa5
Nina19980108 <ninagirl8787@gmail.com>
2021-05-20 16:25:57 +0800

    feat: modify MONGODB_URI

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const MONGODB_URI = 'mongodb+srv://expense_tracker:aaa@cluster0.20zai.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/record'
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const db = mongoose.connection
