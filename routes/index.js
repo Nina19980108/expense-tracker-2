fe375271aefbe3ee9db0ddeb9cf2f128e89ed64b
Nina19980108 <ninagirl8787@gmail.com>
2021-06-01 16:23:00 +0800

    feat: add users module

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,11 @@ const router = express.Router()
 const home = require('./modules/home')
 const New = require('./modules/new')
 const edit = require('./modules/edit')
+const users = require('./modules/users')
 
 router.use('/', home)
 router.use('/new', New)
 router.use('/edit', edit)
+router.use('/users', users)
 
 module.exports = router
