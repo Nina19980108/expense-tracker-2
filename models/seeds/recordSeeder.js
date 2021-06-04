475a09f19109336c4420d111316e974d467c297a
Nina19980108 <ninagirl8787@gmail.com>
2021-06-04 10:48:33 +0800

    feat: modify record seeder

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,5 +1,12 @@
+const bcrypt = require('bcryptjs')
+require('dotenv')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv')
+}
 const db = require('../../config/mongoose')
 const Record = require('../record')
+const User = require('../user')
 
 const expenses = [
   ['tissue', 'fa-home', '2021-04-22', 254, 'costco'],
@@ -15,13 +22,41 @@ const expenses = [
   merchant: expense[4]
 }))
 
+const SEED_USER = {
+  name: 'root',
+  email: 'root@email.com',
+  password: 'root'
+}
+
 db.once('open', () => {
-  Record.create(expenses)
-    .then(() => {
-      console.log('insert expenses done!')
-      return db.close()
+  bcrypt
+    .genSalt(10)
+    .then(salt => bcrypt.hash(SEED_USER.password, salt))
+    .then(hash => {
+      return User.create({
+        name: SEED_USER.name,
+        email: SEED_USER.email,
+        password: hash
+      })
+    })
+    .then(user => {
+      const userId = user._id
+      return Promise.all(Array.from(
+        { length: expenses.length },
+        (_, i) => {
+          return Record.create({
+            name: expenses[i].name,
+            category: expenses[i].category,
+            date: expenses[i].date,
+            amount: expenses[i].amount,
+            merchant: expenses[i].merchant || null,
+            userId
+          })
+        }
+      ))
     })
     .then(() => {
-      console.log('record datebase close!')
+      console.log('done.')
+      process.exit()
     })
 })
