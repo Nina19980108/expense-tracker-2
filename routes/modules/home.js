b088cd87e889911c679c811320b0bf3ed98705f3
Nina19980108 <ninagirl8787@gmail.com>
2021-06-06 19:52:22 +0800

    feat: add search month feature

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const record = require('../../models/record')
 const router = express.Router()
 
 const Record = require('../../models/record')
@@ -17,7 +18,7 @@ router.get('/', (req, res) => {
 
   Record.find({ userId })
     .lean()
-    .sort({ _id: 'asc' })
+    .sort({ date: 'desc' })
     .then(record => res.render('index', { record, totalAmount, userId }))
     .catch(error => console.error(error))
 })
@@ -31,8 +32,9 @@ router.delete('/:id', (req, res) => {
     .catch(error => console.error(error))
 })
 
-router.get('/category/:id', (req, res) => {
-  const category = req.query.category
+router.get('/selected/:id', (req, res) => {
+  const { category } = req.query
+  const month = req.query.month || ''
 
   let fa_home = false
   let fa_shuttle_van = false
@@ -44,32 +46,95 @@ router.get('/category/:id', (req, res) => {
     return res.redirect('/')
   }
 
-  if (category === 'fa-home') {
-    fa_home = true
-  } else if (category === 'fa-shuttle-van') {
-    fa_shuttle_van = true
-  } else if (category === 'fa-grin-beam') {
-    fa_grin_beam = true
-  } else if (category === 'fa-utensils') {
-    fa_utensils = true
-  } else if (category === 'fa-pen') {
-    fa_pen = true
+  switch (category) {
+    case 'fa-home':
+      fa_home = true
+      break
+    case 'fa-shuttle-van':
+      fa_shuttle_van = true
+      break
+    case 'fa-grin-beam':
+      fa_grin_beam = true
+      break
+    case 'fa-utensils':
+      fa_utensils = true
+      break
+    case 'fa-pen':
+      fa_pen = true
+      break
+  }
+
+  let JAN = false
+  let FEB = false
+  let MAR = false
+  let APR = false
+  let MAY = false
+  let JUN = false
+  let JUL = false
+  let AUG = false
+  let SEP = false
+  let OCT = false
+  let NOV = false
+  let DEC = false
+
+  switch (month) {
+    case '01':
+      JAN = true
+      break
+    case '02':
+      FEB = true
+      break
+    case '03':
+      MAR = true
+      break
+    case '04':
+      APR = true
+      break
+    case '05':
+      MAY = true
+      break
+    case '06':
+      JUN = true
+      break
+    case '07':
+      JUL = true
+      break
+    case '08':
+      AUG = true
+      break
+    case '09':
+      SEP = true
+      break
+    case '10':
+      OCT = true
+      break
+    case '11':
+      NOV = true
+      break
+    case '12':
+      DEC = true
+      break
   }
 
   const query = new RegExp(category)
   let totalAmount = 0
-  let allRecord = false
   const userId = req.params.id
-  Record.find({ userId })
-    .then(record => {
-      if (record) allRecord = true
-      return
-    })
-    .catch(err => console.err(err))
 
   Record.find({ $or: [{ category: query }] })
     .find({ userId })
     .lean()
+    .then(record => {
+      if (month === 'Month') {
+        return record
+      }
+      record = record.filter(rec => {
+        const [dateYear, dateMon] = rec.date.split('-')
+        if (month === dateMon) {
+          return rec
+        }
+      })
+      return record
+    })
     .then(record => {
       record.forEach(record => {
         totalAmount += record.amount
@@ -79,8 +144,21 @@ router.get('/category/:id', (req, res) => {
 
   return Record.find({ $or: [{ category: query }] })
     .find({ userId })
+    .sort({ date: 'desc' })
     .lean()
-    .then(record => res.render('index', { record, allRecord, userId, totalAmount, fa_home, fa_shuttle_van, fa_grin_beam, fa_utensils, fa_pen }))
+    .then(record => {
+      if (month === 'Month') {
+        return record
+      }
+      record = record.filter(rec => {
+        const [dateYear, dateMon] = rec.date.split('-')
+        if (month === dateMon) {
+          return rec
+        }
+      })
+      return record
+    })
+    .then(record => res.render('index', { record, userId, totalAmount, fa_home, fa_shuttle_van, fa_grin_beam, fa_utensils, fa_pen, JAN, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC }))
     .catch(error => console.error(error))
 })
 
